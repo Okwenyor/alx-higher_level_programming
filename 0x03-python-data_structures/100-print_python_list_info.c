cf8b06bb49d1649dfb4f4a167b6f61d90f59832e
Okwenyor <Victorial.apedoh@gmail.com>
2023-08-14 20:45:29 -0700

    python list

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -6,3 +6,16 @@
 * @p: the object to get the info from
 */
 void print_puthon_list_infor(PyObject *p)
+{
+	Py_ssize_t size = PyList_Size(p);
+	Py_ssize-t i;
+
+	printf("[*] Size of the Python List = %zd\n", ((PyListObject *)p)->allocated);
+
+	for (i = 0; i < size; i++)
+	{
+		printf('Element %zd: '',i);
+		PyObject_Print(PyList_GetItem(p, i), stdout, Py_PRINT_RAW);
+		printf("\n");
+	}
+}
