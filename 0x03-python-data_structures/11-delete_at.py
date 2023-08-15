4167d7238b9b59c2fc94d5bdf39d00f9b5f5c04b
Okwenyor <Victorial.apedoh@gmail.com>
2023-08-14 19:47:14 -0700

    python script correction

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python3
-def delete_at(my_list=[], idx=0):
-<<<<<<< HEAD
+
+
+def delete_at(my_list=[]. idx=0):
+
     if idx < 0 or idx > len(my_list) - 1:
         return my_list
-    del_value = my_list[idx]
+
+    del_val = my_list[idx]
     for i in range(idx + 1, len(my_list)):
         my_list[i - 1] = my_list[i]
 
     del(my_list[-1])
+
     return my_list
-=======
-	if idx < 0 or idx > len(my_list) - 1:
-		return my_list
-
-	del_value = my_list[idx]
-	for i in range(idx + 1, len(my_list)):
-	my_list[i - 1] = my_list[i]
-
-del(my_list[-1])
-return my_list
->>>>>>> ab979adf2d4b06e5e37246661606c941d32d746f
