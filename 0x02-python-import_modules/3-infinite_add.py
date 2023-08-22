92500ea9d224a8b159a990f11b86bada7cbe6e1d
Okwenyor <Victorial.apedoh@gmail.com>
2023-08-22 08:48:51 -0700

    infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,9 +3,9 @@
 import sys
 
 if __name__ == "__main__":
-    total = 0
+    plus = 0
 
     for i in range(1, len(sys.argv)):
-        total += int(sys.argv[i])
+        plus += int(sys.argv[i])
 
-        print(total)
+        print(plus)
