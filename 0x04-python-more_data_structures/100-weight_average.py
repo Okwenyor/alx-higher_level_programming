b55012d1b0852e0ee7c3123e010d501c99694e6f
Okwenyor <Victorial.apedoh@gmail.com>
2023-08-25 04:55:13 -0700

    Squared by using map

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,7 +3,7 @@ def weight_average(my_list=[]):
     if not my_list:
         return 0
 
-    value = sum(score *weight for score, weight in my_list)
+    value = sum(score * weight for score, weight in my_list)
     value2 = sum(weight for score, weight in my_list)
 
     return value/value2
