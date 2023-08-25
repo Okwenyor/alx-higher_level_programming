e082df298e9e4a8e25b1cf157a58b3f9fb2d7a3d
Okwenyor <Victorial.apedoh@gmail.com>
2023-08-25 04:43:57 -0700

    Weighted aversge

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    value = sum(score *weight for score, weight in my_list)
+    value2 = sum(weight for score, weight in my_list)
+
+    return value/value2
