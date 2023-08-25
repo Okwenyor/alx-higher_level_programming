80061a8a5b9f93099f7bc381aaa3f64c91321da9
Okwenyor <Victorial.apedoh@gmail.com>
2023-08-24 20:20:02 -0700

    Multiply by 2

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    '''
+    uniq_num = set()
+    sum_total = 0
+
+    for i in my_list:
+        if i not in uniq_num:
+            sum_total = i
+            uniq_num.add9i)
+        return sum_total '''
+
+
+new_list = sum(set(my_list))
+return new_list
