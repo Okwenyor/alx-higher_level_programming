40b9da19c25f50f33eff9b1314ac1a26bb9b459b
Okwenyor <Victorial.apedoh@gmail.com>
2023-08-30 08:39:23 -0700

    Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -44,7 +44,7 @@ class Square:
         elif type(value[1]) is not int or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
-            self.__position = value      #tuple contains 2 positive integers
+            self.__position = value     # tuple contains 2 positive integers
 
     def area(self):
         """ Computes area of a square """
