5335780a4fc95d3202b6dec1198799eedf83530c
Okwenyor <Victorial.apedoh@gmail.com>
2023-08-29 20:41:07 -0700

    try work

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Creating a class called Square"""
+
+
+class Square:
+    """ Declares a square class with a private instance attribute 'size"""
+
+
+def __init__(self, size):
+    """Constructor method to initialize the square with a given size"""
+    self.__size = size
+
+
+def get_size(self):
+    """Returns the value of the private instance attribute 'size"""
+    return self.__size
+
+
+def set_size(self, size):
+    """Sets the value of the private instance attribute 'size'."""
+    self.__size = size
