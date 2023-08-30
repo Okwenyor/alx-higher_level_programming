7262c03c488c26634ac67abe1f40c2134c4f569f
Okwenyor <Victorial.apedoh@gmail.com>
2023-08-29 22:51:47 -0700

    fixed work

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Creating a class called Square that has some attributes """
+
+
+class Square:
+    """ Creation of the class """
+
+    def __init__(self, size):
+        """ Definition of the init method which contains a private instant
+        called size which represents the size of the square"""
+
+        self.__size = size
